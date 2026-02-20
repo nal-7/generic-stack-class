7fccb13c44e5d90f28fa03a08d3c43d6b3c8e040
nal7 <nal@student.tgm.ac.at>
2026-02-20 13:32:41 +0100

    Added the StackFullException

--- src/StackFullException.java
+++ src/StackFullException.java
@@ -1,6 +1,6 @@
 public class StackFullException extends Exception {
-    public StackFullException(String message) {
-        super(message);
+    public StackFullException(String m) {
+        super(m);
     }
 }
 
