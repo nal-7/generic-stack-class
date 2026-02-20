8c3578a28b7f4f39606ddedff5050f6467f43f26
nal7 <nal@student.tgm.ac.at>
2026-02-20 13:34:11 +0100

    Added the Test class with the corrected comments

--- src/StackTest.java
+++ src/StackTest.java
@@ -1,5 +1,6 @@
 public class StackTest {
     public static void main(String[] args) {
+        // --- Test Scenario 1: Number Stack ---
         System.out.println("Testing Number Stack:");
         Stack<Integer> numberStack = new Stack<>(3);
         try {
@@ -10,14 +11,17 @@ public class StackTest {
             System.out.println("Peek: " + numberStack.peek()); // 30
             System.out.println("Pop: " + numberStack.pop());   // 30
             System.out.println("After pop: " + numberStack.list()); // 10;20
+
+            // Trigger StackFullException
             numberStack.push(40);
-            numberStack.push(50);
+            numberStack.push(50); // This should fail
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
 
         System.out.println("\n---------------------------\n");
 
+        // --- Test Scenario 2: Text Stack ---
         System.out.println("Testing Text Stack:");
         Stack<String> textStack = new Stack<>(5);
         try {
@@ -26,10 +30,11 @@ public class StackTest {
             textStack.push("Java");
             System.out.println("Stack content: " + textStack.list());
 
+            // Empty the stack to test StackEmptyException
             textStack.pop();
             textStack.pop();
             textStack.pop();
-            textStack.pop();
+            textStack.pop(); // This should fail
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
